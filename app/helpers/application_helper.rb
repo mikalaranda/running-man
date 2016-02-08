38f317593d287f907a4debd7c851b9e366192272
Michael Aranda <michaelaranda0@gmail.com>
2016-02-08 00:38:36 +0000

    Provide titles + add season greeting helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+    
+    def get_season_greeting()
+       time = Time.new
+       if(time.month >= 3) && (time.month <= 5)
+           "Happy Spring!"
+       elsif(time.month > 5) && (time.month <= 8)
+           "Happy Summer!"
+       elsif(time.month > 8) && (time.month <= 10)
+           "Happy Autumn!"
+       else
+           "Happy Winter!"
+       end
+    end
+    
 end
